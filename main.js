e50a8c374f1d6aaa3ab4a59a38dfc7378bc256ce
Andrew Allen <achew22@gmail.com>
2010-01-18 06:54:00 +0800

    Update to match current version of node (as of Jan 17th 2010) and also allow definition of proxy end point

--- main.js
+++ main.js
@@ -1,34 +1,50 @@
-var sys = require("sys");
-var http = require("http");
+var sys = require("sys"),
+   http = require("http");
+
+/**
+ * I wanted to make a couchdb proxy, so this is it
+ */
 http.createServer(
   function (client_request, client_response) {
-    var r = client_request.uri.full.split('://')[0];
-    var path = client_request.uri.full.replace(r+'://','', 1);
-    var server = path.split('/')[0];
-    var path = path.replace(server,'',1);
-    if (server.indexOf(':') != -1) {
-      var server = server.split(':');
-      var port = parseInt(server[1]);
-      var server = server[0];
-    } else {
-      var port = 80;
-    }
+    var path = client_request.url;
+    var server = "127.0.0.1";
+    var port = 5984;
     
-    var c = http.createClient(port, server);    
+    var c = http.createClient(port, server);
     var headers = client_request.headers;
     headers['host'] = server+':'+port;
-    var proxy_request = c[client_request.method.toLowerCase()](path, headers);
+    var proxy_request = c.request(client_request.method.toUpperCase(), path, headers);
     client_request.addListener("body", function(chunk) {
       proxy_request.sendBody(chunk);
     });
     
     proxy_request.finish(function (resp) {
       client_response.sendHeader(resp.statusCode, resp.headers);
-
-      encoding = resp.headers['content-encoding'];
-      if (resp.headers['content-encoding'] == 'gzip' || resp.headers['content-type'].indexOf('image') != -1) {
+ 
+      // Don't send binary unless it is necessary
+      sendBinary = false;
+      
+      // Check to see if there are any images or gzip encodings to send
+      try {
+          if (resp.headers['content-encoding'] == 'gzip') {
+            sendBinary = true;
+          }      
+      } catch (err) {
+        // There is no content-encoding
+      }
+      try {
+          if (resp.headers['content-type'].indexOf('image') != -1) {
+            sendBinary = true;
+          }
+      } catch (err) {
+        // There is no content-type
+      }
+      
+      if (sendBinary) {
         resp.setBodyEncoding('binary');
         encoding = 'binary';
+      } else {
+        encoding = 'ascii';
       }
       
       resp.addListener("body", function(chunk) {
@@ -39,4 +55,3 @@ http.createServer(
     }
   ).listen(8000);
 sys.puts("Server running at http://127.0.0.1:8000/");
-
