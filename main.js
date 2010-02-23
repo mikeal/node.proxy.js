c0d5eea7f2aae772e2b1b81300bfba49ff6c1669
Mikeal Rogers <mikeal.rogers@gmail.com>
2010-02-22 17:01:59 -0800

    Fixing for all the new changes in node.

--- main.js
+++ main.js
@@ -1,42 +1,61 @@
-var sys = require("sys");
-var http = require("http");
-http.createServer(
-  function (client_request, client_response) {
-    var r = client_request.uri.full.split('://')[0];
-    var path = client_request.uri.full.replace(r+'://','', 1);
-    var server = path.split('/')[0];
-    var path = path.replace(server,'',1);
-    if (server.indexOf(':') != -1) {
-      var server = server.split(':');
-      var port = parseInt(server[1]);
-      var server = server[0];
-    } else {
-      var port = 80;
-    }
-    
-    var c = http.createClient(port, server);    
-    var headers = client_request.headers;
-    headers['host'] = server+':'+port;
-    var proxy_request = c[client_request.method.toLowerCase()](path, headers);
-    client_request.addListener("body", function(chunk) {
-      proxy_request.sendBody(chunk);
-    });
-    
-    proxy_request.finish(function (resp) {
-      client_response.sendHeader(resp.statusCode, resp.headers);
+var sys = require("sys"),
+    http = require("http"),
+    url = require("url");
 
-      encoding = resp.headers['content-encoding'];
-      if (resp.headers['content-encoding'] == 'gzip' || resp.headers['content-type'].indexOf('image') != -1) {
-        resp.setBodyEncoding('binary');
-        encoding = 'binary';
-      }
-      
-      resp.addListener("body", function(chunk) {
-        client_response.sendBody(chunk, encoding);
-      });
-      resp.addListener("complete", function() {client_response.finish()});
-    });
-    }
-  ).listen(8000);
-sys.puts("Server running at http://127.0.0.1:8000/");
+binaryContentTypes = ['application/octet-stream', 'application/ogg', 'application/zip', 'application/pdf',
+                      'image/gif', 'image/jpeg', 'image/png', 'image/tiff', 'image/vnd.microsoft.icon',
+                      'multipart/encrypted', 'application/vnd.ms-excel', 'application/vnd.ms-powerpoint',
+                      'application/msword', 'application/x-dvi', 'application/x-shockwave-flash', 
+                      'application/x-stuffit', 'application/x-rar-compressed', 'application/x-tar']
 
+var guessEncoding = function (contentEncoding, contentType) {
+  var encoding = "ascii";
+  if (contentEncoding == 'gzip') {
+    encoding = "binary";
+  } else if (contentType) {
+    if (contentType.slice(0,6) == 'video/' || contentType.slice(0,6) == 'audio/') {
+      encoding = "binary";
+    } else if (binaryContentTypes.indexOf(contentType) != -1) {
+      encoding = "binary";
+    }
+  }
+  return encoding;
+}
+
+var requestHandler = function (clientRequest, clientResponse) {
+  var uri = url.parse(clientRequest.url);
+  // var c = pool.getClient(uri.port, uri.hostname);
+  if (uri.port == undefined) {
+    uri.port = {"http:":80,"https:":443}[uri.protocol]
+  }
+  var c = http.createClient(uri.port, uri.hostname)
+  var proxyRequest = c.request(clientRequest.method, uri.pathname, clientRequest.headers);
+  
+  var messageBuffer = ''
+  
+  
+  proxyRequest.addListener("response", function (response) {
+    clientResponse.writeHeader(response.statusCode, response.headers);
+    var encoding = guessEncoding(response.headers['content-encoding'], response.headers['content-type']);
+    response.setBodyEncoding(encoding)
+    response.addListener("data", function (chunk) {
+      clientResponse.write(chunk, encoding);
+    })
+    response.addListener("end", function () {
+      clientResponse.close();
+    })
+  })
+  
+  var encoding = guessEncoding(clientRequest.headers['content-encoding'], clientRequest.headers['content-type']);
+  clientRequest.addListener("data", function (chunk) {
+    proxyRequest.write(chunk, encoding);
+  })
+  clientRequest.addListener("end", function () {
+    proxyRequest.close();
+  })
+  
+}
+
+var server = http.createServer(requestHandler);
+server.listen(8000);
+sys.puts("Server running at http://127.0.0.1:8000/")
